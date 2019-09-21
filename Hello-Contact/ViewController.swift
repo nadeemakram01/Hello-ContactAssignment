a32acb91b6840f86d6e9f9c7f5064924eeabf87b
Nadeem Akram <nadeem.akram01@stclairconnect.ca>
2019-09-21 16:27:44 -0400

    Initial Commit

---
+++ Hello-Contact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello-Contact
+//
+//  Created by Nadeem Akram on 2019-09-21.
+//  Copyright © 2019 Nadeem Akram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
