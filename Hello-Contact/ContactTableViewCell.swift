a638a9014d0f331fa77a950fe50dc867711374bc
Nadeem Akram <nadeem.akram01@stclairconnect.ca>
2019-09-21 17:38:52 -0400

    Creating the table-view cell subclass

---
+++ Hello-Contact/ContactTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContactTableViewCell.swift
+//  Hello-Contact
+//
+//  Created by Nadeem Akram on 2019-09-21.
+//  Copyright © 2019 Nadeem Akram. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var contactImage: UIImageView!
+    
+
+}
