f87d8c984f16a91da316d2d6dfb4e8061a5f8b5a
Nadeem Akram <nadeem.akram01@stclairconnect.ca>
2019-09-21 17:12:38 -0400

    Fetching a users contacts

--- Hello-Contact/ViewController.swift
+++ Hello-Contact/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import Contacts
 
 class ViewController: UIViewController {
 
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let store = CNContactStore()
+        let authorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
+        
+        if authorizationStatus == .notDetermined {
+            store.requestAccess(for: .contacts) { [weak self] didAuthorize, error in
+                if didAuthorize {
+                    self?.retrieveContacts(from: store)
+                }
+            }
+        } else if authorizationStatus == .authorized  {
+            retrieveContacts(from: store)
+        }
+        
+        
+    }
+    
+    func retrieveContacts(from store: CNContactStore) {
+        let containerId = store.defaultContainerIdentifier()
+        let predicate = CNContact.predicateForContactsInContainer(withIdentifier: containerId)
+        let keysToFetch = [CNContactGivenNameKey as CNKeyDescriptor,
+                           CNContactFamilyNameKey as CNKeyDescriptor,
+                           CNContactImageDataAvailableKey as CNKeyDescriptor,
+                           CNContactImageDataKey as CNKeyDescriptor]
+        
+        
+        
+         let contacts = try! store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch)
+        
+        print(contacts)
+        
+        }
+        
+        
     }
 
-
-}
 
