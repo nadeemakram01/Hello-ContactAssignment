8fc25a851ad049a47d2ba0750a9d77bec446636c
Nadeem Akram <nadeem.akram01@stclairconnect.ca>
2019-09-23 10:29:23 -0400

    Chapter2 - Converting from a table view to a collection view

--- Hello-Contact/ViewController.swift
+++ Hello-Contact/ViewController.swift
@@ -11,7 +11,9 @@ import Contacts
 
 class ViewController: UIViewController {
     var contacts = [Contact]()
-    @IBOutlet var tableView: UITableView!
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,10 +31,7 @@ class ViewController: UIViewController {
             retrieveContacts(from: store)
         }
         
-        tableView.delegate = self
-        tableView.dataSource = self
         
-        navigationItem.rightBarButtonItem = editButtonItem
     }
     
     func retrieveContacts(from store: CNContactStore) {
@@ -43,15 +42,7 @@ class ViewController: UIViewController {
                            CNContactImageDataAvailableKey as CNKeyDescriptor,
                            CNContactImageDataKey as CNKeyDescriptor]
         
-        //    let tmpcontacts = try! store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch)
-        //.map { Contact(contact: $0) }
         
-        // print(tmpcontacts)
-        
-        //    contacts = tmpcontacts.map(Contact.init)
-        //
-        //    DispatchQueue.main.async { [weak self] in
-        //      self?.tableView.reloadData()
         
         contacts = try! store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch).map { Contact (contact: $0)
             
@@ -60,77 +51,5 @@ class ViewController: UIViewController {
         
     }
     
-    
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        
-        tableView.setEditing(editing, animated: animated)
-    }
 }
-
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactTableViewCell") as! ContactTableViewCell
-        let contact = contacts[indexPath.row]
-        
-        cell.nameLabel.text = "\(contact.givenName) \(contact.familyName)"
-        
-        contact.fetchImageIfNeeded { image in
-            cell.contactImage.image = image
-        }
-        
-        return cell
-    }
-}
-
-extension ViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let contact = contacts[indexPath.row]
-        let alertController = UIAlertController(title: "Contact tapped",
-                                                message: "You tapped \(contact.givenName)", preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "Ok", style: .default, handler: { action in
-            tableView.deselectRow(at: indexPath, animated: true)
-        })
-        
-        alertController.addAction(dismissAction)
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    
-    func tableView(_ tableView: UITableView,
-                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        
-        let deleteHandler: UIContextualAction.Handler = { [weak self] action, view, callback in
-            self?.contacts.remove(at: indexPath.row)
-            self?.tableView.beginUpdates()
-            self?.tableView.deleteRows(at: [indexPath], with: .fade)
-            self?.tableView.endUpdates()
-            callback(true)
-        }
-        
-        let deleteAction = UIContextualAction(style: .destructive,
-                                              title: "Delete", handler: deleteHandler)
-        
-        return UISwipeActionsConfiguration(actions: [deleteAction])
-    }
-    
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let contact = contacts.remove(at: sourceIndexPath.row)
-        contacts.insert(contact, at: destinationIndexPath.row)
-    }
-}
-
-extension ViewController: UITableViewDataSourcePrefetching {
-    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-        for indexPath in indexPaths {
-            let contact = contacts[indexPath.row]
-            contact.fetchImageIfNeeded()
-        }
-    }
-}
-
-
