836df459ff434e0c8142b4428eb23c363aea32a1
Nadeem Akram <nadeem.akram01@stclairconnect.ca>
2019-09-23 22:30:31 -0400

    Enabling cell selection for your collection view

--- Hello-Contact/ViewController.swift
+++ Hello-Contact/ViewController.swift
@@ -114,5 +114,21 @@ extension ViewController:UICollectionViewDelegateFlowLayout {
     
     
 }
+
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? ContactCollectionViewCell
+            else { return }
+        
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseOut], animations: {
+            cell.contactImage.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseIn], animations: {
+                cell.contactImage.transform = .identity
+            }, completion: nil)
+        })
+    }
+}
     
 
