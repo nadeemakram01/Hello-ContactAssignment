45b274a800f71770a2b14459308df3ebc77c5f8e
Nadeem Akram <nadeem.akram01@stclairconnect.ca>
2019-09-23 22:05:18 -0400

    Implementing CollectionViewContentSize

---
+++ Hello-Contact/Contact.swift
@@ -0,0 +1,47 @@
+//
+//  Contact.swift
+//  Hello-Contact
+//
+//  Created by Nadeem Akram on 2019-09-23.
+//  Copyright © 2019 Nadeem Akram. All rights reserved.
+//
+
+import UIKit
+import Contacts
+
+class Contact {
+    private let contact: CNContact
+    var image: UIImage?
+    
+    var givenName: String {
+        return contact.givenName
+    }
+    
+    var familyName: String {
+        return contact.familyName
+    }
+    
+    init(contact: CNContact) {
+        self.contact = contact
+    }
+    
+    func fetchImageIfNeeded(completion: @escaping ((UIImage?) -> Void) = { _ in }) {
+        guard contact.imageDataAvailable == true, let imageData = contact.imageData else {
+            completion(nil)
+            return
+        }
+        
+        if let image = self.image {
+            completion(image)
+            return
+        }
+        
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            self?.image = UIImage(data: imageData)
+            DispatchQueue.main.async {
+                completion(self?.image)
+            }
+        }
+    }
+}
+
