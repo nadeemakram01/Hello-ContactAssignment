474e649cce8325e3cd715a97721095179df18e0a
Nadeem Akram <nadeem.akram01@stclairconnect.ca>
2019-09-26 18:46:20 -0400

    Improving the regular size layout

--- Hello-Contact/ContactDetailViewController.swift
+++ Hello-Contact/ContactDetailViewController.swift
@@ -14,12 +14,30 @@ class ContactDetailViewController: UIViewController {
     
     @IBOutlet var contactImage: UIImageView!
     @IBOutlet var contactNameLabel: UILabel!
-
-    override func viewDidLoad() {
+    var compactWidthConstraint: NSLayoutConstraint!
+      var compactHeightConstraint: NSLayoutConstraint!
+      var regularWidthConstraint: NSLayoutConstraint!
+      var regularHeightConstraint: NSLayoutConstraint!
+      
+      @IBOutlet var contactPhoneLabel: UILabel!
+      @IBOutlet var contactEmailLabel: UILabel!
+      @IBOutlet var contactAddressLabel: UILabel!
+    
+      var contact: Contact?
+      
+      override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
+        if let contact = self.contact {
+          contact.fetchImageIfNeeded { [weak self] image in
+            self?.contactImage.image = image
+          }
+          
+          contactNameLabel.text = "\(contact.givenName) \(contact.familyName)"
+          contactPhoneLabel.text = contact.phoneNumber
+          contactEmailLabel.text = contact.emailAddress
+          contactAddressLabel.text = contact.address
+        }
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear),
                                                name: UIApplication.keyboardWillShowNotification,
@@ -29,10 +47,7 @@ class ContactDetailViewController: UIViewController {
                                                name: UIApplication.keyboardWillHideNotification,
                                                object: nil)
         
-    }
-    
-    
-    let views: [String: Any] = ["contactImage": contactImage, "contactNameLabel": contactNameLabel]
+        let views: [String: Any] = ["contactImage": contactImage, "contactNameLabel": contactNameLabel]
     
         var allConstraints = [NSLayoutConstraint]()
     
@@ -61,30 +76,44 @@ class ContactDetailViewController: UIViewController {
     
         NSLayoutConstraint.activate(allConstraints)
       }
-    
-    @objc func keyboardWillAppear(_ notification: Notification) {
-      guard let userInfo = notification.userInfo,
-        let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
-        let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
-        else { return }
       
-      scrollViewBottomConstraint.constant = keyboardFrame.cgRectValue.size.height
-      UIView.animate(withDuration: TimeInterval(animationDuration), animations: { [weak self ] in
-        self?.view.layoutIfNeeded()
-      })
-    }
-    
-    @objc func keyboardWillHide(_ notification: Notification) {
-      guard let userInfo = notification.userInfo,
-        let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
-        else { return }
+      @objc func keyboardWillAppear(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+          let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+          let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+          else { return }
+        
+        scrollViewBottomConstraint.constant = keyboardFrame.cgRectValue.size.height
+        UIView.animate(withDuration: TimeInterval(animationDuration), animations: { [weak self ] in
+          self?.view.layoutIfNeeded()
+        })
+      }
       
-      scrollViewBottomConstraint.constant = 0
-      UIView.animate(withDuration: TimeInterval(animationDuration), animations: { [weak self ] in
-        self?.view.layoutIfNeeded()
-      })
+      @objc func keyboardWillHide(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+          let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+          else { return }
+        
+        scrollViewBottomConstraint.constant = 0
+        UIView.animate(withDuration: TimeInterval(animationDuration), animations: { [weak self ] in
+          self?.view.layoutIfNeeded()
+        })
+      }
+      
+      override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        guard let previousTraitCollection = previousTraitCollection,
+           (previousTraitCollection.horizontalSizeClass != traitCollection.horizontalSizeClass ||
+            previousTraitCollection.verticalSizeClass != traitCollection.verticalSizeClass)
+          else { return}
+        
+        if traitCollection.horizontalSizeClass == .regular && traitCollection.verticalSizeClass == .regular {
+          NSLayoutConstraint.deactivate([compactHeightConstraint, compactWidthConstraint])
+          NSLayoutConstraint.activate([regularHeightConstraint, regularWidthConstraint])
+        } else {
+          NSLayoutConstraint.deactivate([regularHeightConstraint, regularWidthConstraint])
+          NSLayoutConstraint.activate([compactHeightConstraint, compactWidthConstraint])
+        }
+      }
     }
-
-    
-
-}
